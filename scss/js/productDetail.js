7b2ecf98376b5cbd336110c1e2b090f270854020
davidChen <73978886+austin0929@users.noreply.github.com>
2023-02-24 18:19:22 +0800

    Update productDetail.js

--- scss/js/productDetail.js
+++ scss/js/productDetail.js
@@ -197,7 +197,6 @@ const getCartList = () => {
 //購物車post到cart
 productsDetail.addEventListener("click", e => {
     e.preventDefault()
-    if (getLocalUserId() == null) { Swal.fire('請先登入', '登入後操作', 'error') return }
     let getId = e.target.getAttribute("data-addCartId")
     if (e.target.classList.contains("js-cartId")) {
         postCart(getId)
